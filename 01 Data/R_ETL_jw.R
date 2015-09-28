329ca6f227451ee4edc54cd04ccc9bed27b4da15
jennawebb117 <jennawebb@utexas.edu>
2015-09-28 17:43:25 -0500

    R_ETL.R file

--- 01 Data/R_ETL_jw.R
+++ 01 Data/R_ETL_jw.R
@@ -7,10 +7,9 @@ df <- read.csv(file_path, stringsAsFactors = FALSE)
 # Replace "." (i.e., period) with "_" in the column names.
 names(df) <- gsub("\\.+", "_", names(df))
 
-str(df) # Uncomment this and  run just the lines to here to get column types to use for getting the list of measures.
+#str(df) # Uncomment this and  run just the lines to here to get column types to use for getting the list of measures.
 
-measures <- c("Disease", "County", "Sex", )
-#measures <- NA # Do this if there are no measures.
+measures <- c("Disease", "County", "Sex")
 
 # Get rid of special characters in each column.
 # Google ASCII Table to understand the following:
